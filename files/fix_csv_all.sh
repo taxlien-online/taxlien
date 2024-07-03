812b2bb2240f6b182887d6b9b3e077a78f171b01
Anton Dodonov <anton_dodonov@mail.ru>
2024-07-03 10:42:00 +0700

    no comments

---
+++ files/fix_csv_all.sh
@@ -0,0 +1,18 @@
+./fix_csv_county.sh fl columbia Columbia
+./fix_csv_county.sh fl dixie Dixie
+./fix_csv_county.sh fl gadsden Gadsden
+./fix_csv_county.sh fl gilchrist Gilchrist
+./fix_csv_county.sh fl lafayette Lafayette
+./fix_csv_county.sh fl polk Polk
+./fix_csv_county.sh fl putnam Putnam
+./fix_csv_county.sh fl sarasota Sarasota
+./fix_csv_county.sh fl suwannee Suwannee
+
+./fix_csv_county.sh az arapahoe Araphoe
+./fix_csv_county.sh az coconino Coconino
+./fix_csv_county.sh az pima Pima
+
+#./fix_csv_county.sh  md carroll Carroll
+#./fix_csv_county.sh  nj mansfield
+#ocean gate
+#orangecity
